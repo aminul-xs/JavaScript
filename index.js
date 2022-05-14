ef13983510aac833d663be2dd776922df9333d14
Md Aminul Islam <56115259+developeaminul@users.noreply.github.com>
2022-05-14 12:28:19 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+// var hello = "I Love Bangladesh";
+// console.log(hello);
+
+/**
+ * --------------------------------------------
+ * class #2
+ * ----------------------------------------------
+ * JavaScript Output
+ */
+
+/**
+ *
+ * Writing into an HTML element, using innerHTML.
+ * Writing into the HTML output using document.write().
+ * Writing into an alert box, using window.alert().
+ * Writing into the browser console, using console.log().
+ *
+ */
+
+//  console.log("Hello world");
+
+/**
+ * --------------------------------------------
+ * class #3
+ * ----------------------------------------------
+ * JavaScript Statements
+ */
+
+/**
+ * Values, Operators, Expressions, Keywords, and Comments.
+ * for example:
+ * document.getElementById("demo").innerHTML = "Hello Dolly.";
+ *
+ */
+
+//JavaScript Code Blocks
+
+// function myFunction() {
+//    document.getElementById("demo1").innerHTML = "Hello Dolly";
+//    document.getElementById("demo2").innerHTML = "How are you?";
+// }
+
+/**-
+ * --------------------------------------------
+ * class #04
+ * ----------------------------------------------
+ * JavaScript Syntax
+ * JavaScript Expression
+ */
+
+// var x;
+// let y = "Hello";
+// console.log(y);
+
+// An expression is a combination of values, variables, and operators, which computes to a value.
+// For example, 5 * 10 evaluates to 50:
+
+
+
+/**-
+ * --------------------------------------------
+ * class #05
+ * ----------------------------------------------
+ * JavaScript Comments
+ * 
+ */
+ //Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+
+
+/**-
+ * --------------------------------------------
+ * class #06
+ * ----------------------------------------------
+ * JavaScript Variables
+ * 
+ */
+
+
+// for example, in JavaScript variables.
+// var x = 5;
+// var y = 6;
+
+// var z = x + y;
+
+// console.log(z);
+
+/**-
+ * --------------------------------------------
+ * class #07
+ * ----------------------------------------------
+ * JavaScript Variables
+ * 
+ */
