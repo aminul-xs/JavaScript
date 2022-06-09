68ed957f0edd0ec56afc5f7b7aaf373504b8b05b
Aminul Islam <98691903+aminul-xs@users.noreply.github.com>
2022-06-09 17:13:55 +0600

    #debugger

--- index.js
+++ index.js
@@ -701,7 +701,7 @@
  *
  */
 
- const  cars = ["A", "B", "C", "D", "E"];
+//  const  cars = ["A", "B", "C", "D", "E"];
 
 // for(let i = 0; i < cars.length; i++){
 //    text = cars[i] + "\n";
@@ -710,22 +710,25 @@
 
 
 
-function howMany(selectObject) {
-   let numberSelected = 0;
-   for (let i = 0; i < selectObject.options.length; i++) {
+// function howMany(selectObject) {
+//    let numberSelected = 0;
+//    for (let i = 0; i < selectObject.options.length; i++) {
      
      
-      if (selectObject.options[i].selected) {
-       numberSelected++;
-     }
-   }
-   return numberSelected;
- }
+//       if (selectObject.options[i].selected) {
+//        numberSelected++;
+//      }
+//    }
+//    return numberSelected;
+//  }
 
- const btn = document.getElementById('btn');
+//  const btn = document.getElementById('btn');
  
-btn.addEventListener('click', () => {
-  const musicTypes = document.selectForm.musicTypes;
-  console.log(`You have selected ${howMany(musicTypes)} option(s).`);
-});
+// btn.addEventListener('click', () => {
+//   const musicTypes = document.selectForm.musicTypes;
+//   console.log(`You have selected ${howMany(musicTypes)} option(s).`);
+// });
 
+let x = 15 * 5;
+debugger;
+console.log();
