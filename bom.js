0224e32b2381bd8775cc196fa446084e812834db
Md Aminul Islam <mdaminulislam2015bd@gmail.com>
2023-04-03 19:32:51 +0600

    ad

--- bom.js
+++ bom.js
@@ -6,8 +6,112 @@
 *
 */
 
-const WIDTH = document.getElementById('width');
-const HEIGHT = document.getElementById('height');
+// const WIDTH = document.getElementById('width');
+// const HEIGHT = document.getElementById('height');
 
-WIDTH.innerHTML = 'Window Inner width is:' + this.innerWidth;
-HEIGHT.innerHTML = 'Window Inner height is:' + this.innerHeight;
+// WIDTH.innerHTML = 'Window Inner width is:' + this.innerWidth;
+// HEIGHT.innerHTML = 'Window Inner height is:' + this.innerHeight;
+
+// let myWindow;
+
+// function openWindow() {
+//     myWindow = window.open("https://www.google.com/");
+// }
+
+// function closeWindow() {
+//     if (myWindow) {
+//         myWindow.close();
+//         console.log(myWindow);
+
+//     } else {
+//         //console.log('No window found to close.');
+//         // prints the text to the console as an error message
+//         // console.error('This is an error');
+
+//         // prints the text to the console as an informational message
+//          console.info('This is some information');
+
+//         // prints the text to the console as a warning
+//         //console.warn('This is a warning');
+
+//         // prints the geometry of the document body as an object
+//         // console.log(document.body.getBoundingClientRect());
+
+//         // prints the geometry of the document body as a table
+//         // console.table(document.body.getBoundingClientRect());
+
+//         // shows a list of techologies as a collapsed group
+//         // let technologies = ["HTML", "CSS", "SVG", "ECMAScript"];
+//         // console.groupCollapsed('technolgies');
+//         // technologies.forEach(tech => { console.info(tech); })
+
+
+//         //  console.groupEnd('Technolgies');
+
+//     }
+// }
+
+
+
+/**
+* --------------------------------------------
+* Class #104
+* ----------------------------------------------
+* JavaScript cookies
+*
+*/
+
+// function goForward() {
+//     window.history.forward()
+// }
+
+// function goBack() {
+//     window.history.back()
+//   }
+
+/**
+* --------------------------------------------
+* Class #107
+* ---------------------------------------------
+* JavaScript cookies
+*
+*/
+
+// const CONFIRM = document.getElementById("confirm");
+// const PROMPT = document.getElementById('prompt');
+
+// function showAlert() {
+//     window.alert("Hello Bangladesh");
+// }
+
+// function showConfirm() {
+//     let text;
+//     if (window.confirm("Press a button!")) {
+//         text = "You pressed OK!";
+//         console.log(true);
+//     } else {
+//         text = "You pressed Cancel!";
+//         console.log(false);
+//     }
+//     CONFIRM.innerHTML = text;
+// }
+
+// function showPrompt() {
+//     let person = window.prompt("Please enter your name:", "Harry Potter");
+//     let txt;
+//     if (person === null || person === "") {
+//         txt = "User is cancelled the prompt";
+//     } else {
+//         txt = "Hello " + person + "! How are you??"
+//     }
+//     PROMPT.innerHTML = txt;
+// }
+
+/**
+* --------------------------------------------
+* Class #108
+* ---------------------------------------------
+* JavaScript cookies
+*
+*/
+
